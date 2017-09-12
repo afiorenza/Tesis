ba017d5439e2d7e1874edb075fb502d3de53acf8
af <agustinfiorenza@gmail.com>
2017-09-12 09:45:58 -0300

    update lua script

--- redis/set.lua
+++ redis/set.lua
@@ -5,7 +5,6 @@ local lumenes = ARGV[3]
 local grados = ARGV[4]
 local decibelios = ARGV[5]
 local tipoPuesto = ARGV[6]
-local nextIndex
 
 -- Busca valor contador y si no existe lo agrega
 if (redis.call('GET', puesto..':'..area) == nil) then
@@ -13,11 +12,13 @@ if (redis.call('GET', puesto..':'..area) == nil) then
 end
 
 -- Incrementa contador
-nextIndex = redis.call('INCR', puesto..':'..area)
+local nextIndex = redis.call('INCR', puesto..':'..area)
+local nextKey = puesto..':'..area..':'..nextIndex
 
--- Agrega registro
-redis.call('HSET', puesto..':'..area..':'..nextIndex, '')
+-- Agrega registros
+redis.call('HSET', nextKey, KEYS[3], ARGV[3])
+redis.call('HSET', nextKey, KEYS[4], ARGV[4])
+redis.call('HSET', nextKey, KEYS[5], ARGV[5])
+redis.call('HSET', nextKey, KEYS[6], ARGV[6])
 
-lumenes [lumenes] grados [grados] decibelios [decibelios] tipoPuesto [tipoPuesto]
-
-return nextIndex
+return redis.call('HGETALL', nextKey)
