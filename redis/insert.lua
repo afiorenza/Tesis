777cbcc5c81c03551d83ded4678dafbe20599617
af <agustinfiorenza@gmail.com>
2017-09-13 12:02:52 -0300

    major changes

--- redis/insert.lua
+++ redis/insert.lua
@@ -1,43 +1,42 @@
-local parametrosAmbientales = {'lumenes', 'grados', 'decibelios'}
+local parametros = {'area', 'puesto', 'lumenes', 'grados', 'decibelios', 'tipoPuesto'}
 
-local function initializeAreaYPuesto (areaYPuesto)
+local function nuevoNroSecuencial (areaYPuesto)
   if (redis.call('GET', areaYPuesto) == nil) then
     redis.call('SET', areaYPuesto, 0)
   end
+  return redis.call('INCR', areaYPuesto)
 end
 
-local function publish (channel, text)
-  redis.call('PUBLISH', channel, text)
+local function addAndPublish (key, field, value)
+  redis.call('HSET', key, field, value)
+  redis.call('PUBLISH', 'insert', field..': '..value..' agregado en '..key..'.')
 end
 
 local areaYPuesto = KEYS[1]..':'..KEYS[2]
-
-initializeAreaYPuesto(areaYPuesto)
-
-local nextIndex = redis.call('INCR', areaYPuesto)
+local nextIndex = nuevoNroSecuencial(areaYPuesto)
 local nextKey = areaYPuesto..':'..nextIndex
 
-redis.call('HSET', nextKey, 'tipoPuesto', KEYS[6])
+for index = 3, 6 do
+  local valueOfIndexMinusTwo = redis.call('HGET', areaYPuesto..':'..nextIndex - 2, parametros[index])
+  local valueOfIndexMinusOne = redis.call('HGET', areaYPuesto..':'..nextIndex - 1, parametros[index])
+  local currentValue = KEYS[index]
 
-for index = 1, 3 do
-  local valueOfIndexMinusTwo = redis.call('HGET', areaYPuesto..':'..nextIndex - 2, parametrosAmbientales[index])
-  local valueOfIndexMinusOne = redis.call('HGET', areaYPuesto..':'..nextIndex - 1, parametrosAmbientales[index])
-  local currentValue = KEYS[index + 2]
-
-  if (type(tonumber(currentValue)) ~= 'number') then
-    publish('error', 'Valor anomalo '..parametrosAmbientales[index]..' en '..nextKey..'.')
-  else
-    if (
-      valueOfIndexMinusTwo ~= false and valueOfIndexMinusOne ~= false
-      and (valueOfIndexMinusTwo ~= valueOfIndexMinusOne or valueOfIndexMinusOne ~= currentValue)
-      and not (valueOfIndexMinusOne > valueOfIndexMinusTwo and valueOfIndexMinusOne < currentValue)
-    ) then
-      local indexMinusOne = nextIndex - 1
-      redis.call('HDEL', areaYPuesto..':'..indexMinusOne, parametrosAmbientales[index])
-      publish('error', 'Valor fuera de rango '..parametrosAmbientales[index]..' en '..areaYPuesto..':'..indexMinusOne..'.')
+  if (index ~= 6) then
+    if (type(tonumber(currentValue)) ~= 'number') then
+      redis.call('PUBLISH', 'error', 'Valor anomalo '..parametros[index]..' en '..nextKey..'.')
+    else
+      if (
+        valueOfIndexMinusTwo ~= false and valueOfIndexMinusOne ~= false
+        and (valueOfIndexMinusTwo ~= valueOfIndexMinusOne or valueOfIndexMinusOne ~= currentValue)
+        and not (valueOfIndexMinusOne > valueOfIndexMinusTwo and valueOfIndexMinusOne < currentValue)
+      ) then
+        local indexMinusOne = nextIndex - 1
+        redis.call('HDEL', areaYPuesto..':'..indexMinusOne, parametros[index])
+        redis.call('PUBLISH', 'error', 'Valor fuera de rango '..parametros[index]..' en '..areaYPuesto..':'..indexMinusOne..'.')
+      end
+      addAndPublish(nextKey, parametros[index], currentValue)
     end
-
-    redis.call('HSET', nextKey, parametrosAmbientales[index], currentValue)
-    publish('tesis', KEYS[index + 2]..' '..parametrosAmbientales[index]..' agregado en '..nextKey..'.')
+  else
+    addAndPublish(nextKey, parametros[index], currentValue)
   end
 end
